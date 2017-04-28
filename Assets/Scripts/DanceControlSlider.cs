cef7f4b57a07402e1989ee511ffda99763166c80
Anton Sivertsson <sivertsson.anton@gmail.com>
2017-04-28 23:26:11 +0200

    Create beatbar with object pool, restructure assets, start DanceControlSlider

---
+++ Assets/Scripts/DanceControlSlider.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceControlSlider : MonoBehaviour {
+
+	public AudioClip musicClip;
+	public int bpm;
+	public List<AudioClip> beatAccents;
+	public GameObject sliderObject;
+	public GameObject cmdPrefab;
+
+	private float barWidth;
+	private float cmdPrefabWidth;
+	
+
+
+	// Use this for initialization
+	void Start () {
+		cmdPrefabWidth = cmdPrefab.GetComponent<RectTransform>().rect.width;
+		calculateSong(musicClip, bpm);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow) ||
+			Input.GetKeyDown(KeyCode.RightArrow) ||
+			Input.GetKeyDown(KeyCode.UpArrow) ||
+			Input.GetKeyDown(KeyCode.DownArrow)) {
+				DoShit(Input.inputString);
+		}
+	}
+
+	void calculateSong(AudioClip musicClip, int bpm) {
+		float songLength = musicClip.length;
+	}
+
+	void createSong(List<AudioClip> musicClips) {
+		//Creates a song from AudioClips in a list
+		bool hasAccent = false;
+		bool hasIntermission = false;
+
+		AudioClip baseBeat = musicClips[0];
+		if (musicClips.Count > 1) {
+			AudioClip accentBeat = musicClips[1];
+			hasAccent = true;
+		}
+		if (musicClips.Count > 2) {
+			AudioClip intermission = musicClips[0];
+			hasIntermission = true;
+		}
+	}
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+
+	public static ObjectPool current;
+	public GameObject pooledObject;
+	public int pooledAmount = 25;
+
+	List<GameObject> pooledObjects;
+
+	void Awake() {
+		current = this;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		pooledObjects = new List<GameObject>();
+		for(int i = 0; i < pooledAmount; i++) {
+			GameObject obj = (GameObject) Instantiate(pooledObject);
+			obj.SetActive(false);
+			obj.transform.SetParent(GameObject.Find("BeatBar").transform);
+			pooledObjects.Add(obj);
+		}
+		print("Object pool loaded");
+	}
+	
+	// Update is called once per frame
+
+	public GameObject GetPooledObject() {
+		for(int i = 0; i < pooledObjects.Count; i++) {
+			if(!pooledObjects[i].activeInHierarchy) {
+				return pooledObjects[i];
+			}
+		}
+		return null;
+	}
+
+	public GameObject getCustomerObjByIndex(int index) {
+		GameObject customerObject = pooledObjects[index];
+		customerObject.SetActive(true);
+		return customerObject;
+	}
+}
